dfa6afe3fac3f223dc9c6c215f09d6cb1ada4067
RobinGeers <robin.geers@student.howest.be>
2015-06-08 15:52:10 +0200

    Pop-up maak externe werknemer -> Wordt al toegevoegd in PHP

---
+++ ChangeInst/djfqs5dfqs5df46qsd4.php
@@ -0,0 +1,50 @@
+
+
+<?php
+//var url = mylink+"?naam="+myar[0]+"&bedrijf="+myar[1]+"&adres"+myar[2]+"&tel"+myar[3]+"&email"+myar[4];
+$naam = "".$_GET["naam"];
+$bedrijf = "".$_GET["bedrijf"];
+$adres = "".$_GET["adres"];
+$tel = "".$_GET["tel"];
+$email = "".$_GET["email"];
+
+//CODE DIE ERVOOR ZORGT DAT NAAR DATABASE WORDT VERZET
+
+
+$mysqli = new mysqli('mysqlstudent', 'wouterdumoeik9aj', 'zeiSh6sieHuc', 'wouterdumoeik9aj');
+
+//controleren op fouten
+//echo "h";
+if ($mysqli->connect_error)
+{
+    echo "Geen connectie mogelijk met de database";
+    echo  "<script type='text/javascript'>";
+    echo "window.close();";
+    echo "</script>";
+}
+echo "test";
+//$result = $mysqli->query("SELECT userPrincipalName,ROL FROM EmailsLeerkrachten");
+//INSERT INTO table_name (column1,column2,column3,...)
+//VALUES (value1,value2,value3,...);
+$stmt = $mysqli->prepare('INSERT INTO external(Naam, NaamBedrijf, Adres, Telefoon, Emailadres) VALUES(?,?,?,?,?)');
+$stmt->bind_param('sssss',$naam,$bedrijf,$adres,$tel,$email);
+//$stmt->bind_param('s', $naam);
+
+
+$stmt->execute();
+echo "hhh";
+$result = $stmt->affected_rows;
+echo $result;
+/*while ($row = $result->fetch_assoc()) {
+    // do something with $row
+}*/
+
+
+
+$mysqli->close();
+echo "hallo";
+echo  "<script>";
+echo "window.close();";
+echo "</script>";
+
+?>
